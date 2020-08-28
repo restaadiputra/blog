cbd8bf1ca205eac7856456f4f14f26d4a7f1f7bd
Resta Adiputra <resta.adiputra@gmail.com>
2020-08-28 12:12:03 +0700

    add remote image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,7 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          `gatsby-remark-images-anywhere`,
           {
             resolve: `gatsby-remark-highlight-code`,
             options: {
